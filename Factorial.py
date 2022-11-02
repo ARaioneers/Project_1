cbbee9a29e162ef4bf26acde7613c5113fd31624
Anjali <anjali.rajmane@aioneers.com>
2022-11-02 10:52:11 +0530

    first commit

---
+++ Factorial.py
@@ -0,0 +1,11 @@
+def factorial(a):
+    f=1
+    if a>=0:
+        for i in range(1,a+1):
+            f=f*i
+            print(f"Factorial of Number {a} is = ",f)
+    else:
+        print("Factorial is not found")
+        factorial()
+        
+factorial(5)
